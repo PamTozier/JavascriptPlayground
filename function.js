9cb47c1e5b3974023cc9f7acf4cbc4acefc65e92
PamTozier <pamela.tozier@colorado.edu>
2013-09-26 21:22:00 -0600

    Adding First Javascript Sample
    Using the word count function and testing modifications to break it and
    fix it. The code here works

---
+++ function.js
@@ -0,0 +1,19 @@
+document.write('this came from the script');
+document.write('   ');
+document.write('<b>loud</b>');
+
+document.write('<br/> <h4>Word Count example</h4>');
+var wordCount = function(text) {
+ 
+	// \W matches non-word characters, match one or more
+  var tokens = text.split(/\W+/);
+ 
+	// account for empty elements at the start or end of the list
+  var count = tokens.length;
+  
+	if(tokens[0] === '') count--;
+  if(tokens[tokens.length - 1] === '') count--;
+  return count;
+}
+
+document.write('test 1: ' + wordCount('one two , nine and a half three'));
